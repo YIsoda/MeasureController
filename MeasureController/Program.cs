bc6972f153ea09eadfee1439fca845920048ef3b
Yosuke Isoda <y.isoda.3953@gmail.com>
2020-07-15 12:53:19 +0900

    initial commit; add C# Project

---
+++ MeasureController/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MeasureController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
